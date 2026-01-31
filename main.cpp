227ca6b98ecdb16a225988b9687d3f9b3e5a3c1f
Thanapoom Cheewinekkaphop <thanapoomcheewinekkaphop@Thanapooms-Laptop.local>
2026-02-01 06:34:11 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ int main(int argc, char *argv[]) {
     int n = argc - 1;
     if (n <= 0) return 0;
 
-   
+    // allocate dynamic array
     int *pa = new int[n];
     int *pstart = pa;
 
-    
+    // convert argv to int array
     for (int i = 0; i < n; i++) {
         *pa = atoi(argv[i + 1]);
         pa++;
     }
 
-    
+    // reverse using pointers
     pa = pstart;
     int *pb = pstart + n - 1;
     int temp;
@@ -30,11 +30,11 @@ int main(int argc, char *argv[]) {
         pb--;
     }
 
-    
+    // print reversed numbers (TWO spaces between)
     pa = pstart;
     for (int i = 0; i < n; i++, pa++) {
+        if (i > 0) cout << "  ";   // two spaces
         cout << *pa;
-        if (i != n - 1) cout << " ";
     }
     cout << endl;
 
